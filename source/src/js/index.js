1f16fd4807a04494973db740f2daeb4d526e4a2a
Random4eg <panaram452@gmail.com>
2024-05-04 09:22:36 +0300

    last

--- source/src/js/index.js
+++ source/src/js/index.js
@@ -1,5 +1,8 @@
 import projects from '/src/js/modules/projects.js';
 import home from '/src/js/modules/home.js';
+import home from '/src/js/modules/home.js';
+import button from './modules/button';
 
 projects();
 home();
+button();
---
+++ source/src/js/modules/button.js
@@ -0,0 +1,20 @@
+function button() {
+	console.log('123');
+}
+
+window.onscroll = function () {
+	scrollFunction();
+};
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById('myBtn').style.display = 'block';
+	} else {
+		document.getElementById('myBtn').style.display = 'none';
+	}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+export default button;
